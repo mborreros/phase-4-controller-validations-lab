2b4ab02db343b3e4cf208265e2defd1c2c12e6be
mborreros <mlbsindia@gmail.com>
2022-05-06 14:44:36 -0400

    commit tree b1553160e8fcc737803884c7ec1e262b4ab73950

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,12 +6,14 @@ class PostsController < ApplicationController
     render json: post
   end
 
+  # Change PostsController#update to send back the error messages if the updated post is invalid
+
   def update
     post = Post.find(params[:id])
-
-    post.update(post_params)
-
+    post.update!(post_params)
     render json: post
+  rescue ActiveRecord::RecordInvalid => invalid
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
   private
